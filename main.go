ab1d2a5ff07274815e2642052c24513c0960cbb4
Tamal Saha <tamal@appscode.com>
2022-03-02 15:01:07 -0800

    added-all
    Signed-off-by: Tamal Saha <tamal@appscode.com>

--- main.go
+++ main.go
@@ -3,12 +3,12 @@ package main
 import (
 	"context"
 	"fmt"
+	"k8s.io/apimachinery/pkg/api/resource"
 
 	core "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/runtime"
 	clientgoscheme "k8s.io/client-go/kubernetes/scheme"
 	"k8s.io/klog/v2/klogr"
-	"kmodules.xyz/resource-metrics/api"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/client/apiutil"
@@ -56,22 +56,21 @@ func run() error {
 		panic(err)
 	}
 	for _, pod := range pods.Items {
-		fmt.Println(pod.Name)
-
-		var req, cap core.ResourceList
+		req := resource.Quantity{Format: resource.BinarySI}
+		limit := resource.Quantity{Format: resource.BinarySI}
 		for _, vol := range pod.Spec.Volumes {
 			if vol.PersistentVolumeClaim != nil {
 				var pvc core.PersistentVolumeClaim
 				if err := kc.Get(context.TODO(), client.ObjectKey{Namespace: pod.Namespace, Name: vol.PersistentVolumeClaim.ClaimName}, &pvc); err == nil {
-					req = api.AddResourceList(req, pvc.Spec.Resources.Requests)
-					cap = api.AddResourceList(cap, pvc.Status.Capacity)
+					req.Add(*pvc.Spec.Resources.Requests.Storage())
+					limit.Add(*pvc.Status.Capacity.Storage())
 				}
 			}
 		}
 
-		fmt.Println("req: ", req[core.ResourceStorage])
-		fmt.Println("cap: ", cap[core.ResourceStorage])
-		fmt.Println("--------------------------------")
+		fmt.Printf("--------------%s---------------\n", pod.Name)
+		fmt.Println("req: ", req.String())
+		fmt.Println("cap: ", limit.String())
 	}
 
 	return nil
