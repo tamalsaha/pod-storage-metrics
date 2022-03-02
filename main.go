590963d584ac9a9625c63fe6af1de191e49693ac
Tamal Saha <tamal@appscode.com>
2022-03-02 14:10:47 -0800

    added-all
    Signed-off-by: Tamal Saha <tamal@appscode.com>

--- main.go
+++ main.go
@@ -3,15 +3,10 @@ package main
 import (
 	"context"
 	"fmt"
-	//	"time"
 
-	// 	"github.com/nats-io/nats.go"
-	// 	"github.com/tamalsaha/nats-hop-demo/shared"
-	// 	"github.com/tamalsaha/nats-hop-demo/transport"
 	core "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/runtime"
 	clientgoscheme "k8s.io/client-go/kubernetes/scheme"
-	//	"k8s.io/klog/v2"
 	"k8s.io/klog/v2/klogr"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
@@ -21,7 +16,6 @@ import (
 func NewClient() (client.Client, error) {
 	scheme := runtime.NewScheme()
 	_ = clientgoscheme.AddToScheme(scheme)
-	crdinstall.Install(scheme)
 
 	ctrl.SetLogger(klogr.New())
 	cfg := ctrl.GetConfigOrDie()
@@ -43,11 +37,6 @@ func NewClient() (client.Client, error) {
 	})
 }
 
-var (
-	scheme   = runtime.NewScheme()
-	setupLog = ctrl.Log.WithName("setup")
-)
-
 func main() {
 	if err := run(); err != nil {
 		panic(err)
@@ -55,39 +44,7 @@ func main() {
 }
 
 func run() error {
-	_ = clientgoscheme.AddToScheme(scheme)
-
-	// 	nc, err := nats.Connect(shared.NATS_URL)
-	// 	if err != nil {
-	// 		klog.Fatalln(err)
-	// 	}
-	// 	defer nc.Close()
-
-	ctrl.SetLogger(klogr.New())
-	cfg := ctrl.GetConfigOrDie()
-
-	// 	tr, err := cfg.TransportConfig()
-	// 	if err != nil {
-	// 		panic(err)
-	// 	}
-	// 	cfg.Transport, err = transport.New(tr, nc, "k8s", 10000*time.Second)
-	// 	if err != nil {
-	// 		panic(err)
-	// 	}
-
-	mapper, err := apiutil.NewDynamicRESTMapper(cfg)
-	if err != nil {
-		return err
-	}
-
-	c, err := client.New(cfg, client.Options{
-		Scheme: scheme,
-		Mapper: mapper,
-		Opts: client.WarningHandlerOptions{
-			SuppressWarnings:   false,
-			AllowDuplicateLogs: false,
-		},
-	})
+	kc, err := NewClient()
 	if err != nil {
 		return err
 	}
